9135d41c56300000b0dd0ac6cec66296d4666699
AmericoG <99370527+AmericoG@users.noreply.github.com>
2022-02-16 05:00:10 -0300

    Delete src/logicadeprogramacao directory

--- src/logicadeprogramacao/LacosDeRepeticao/Fors/IntroducaoFor.java
+++
@@ -1,8 +0,0 @@
-package logicadeprogramacao.LacosDeRepeticao.Fors;
-
-public class IntroducaoFor {
-    public static void main(String[] args) {
-        for (int numero = 0; numero <=10;) {
-            System.out.println(numero);}
-    }
-}
