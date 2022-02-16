c915c40defaf34f283b932f9ebc081ee219127a8
AmericoG <gabriel.americodc@outlook.com>
2022-02-16 04:54:40 -0300

    teste

--- src/projetojedi/conquista01/Exercicio02.java
+++ src/projetojedi/conquista01/Exercicio02.java
@@ -2,8 +2,8 @@ package projetojedi.conquista01;
 /* Criar duas classes relacionadas (mesmo assunto). Ex: Carro e Moto. */
 
 public class Exercicio02 {
-class facebook {}
-class instagram {}
+class facebook { }
+class instagram { }
 }
 
 
